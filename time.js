e64bdcafc484b78c45444c04ab80550209e7046d
bdankul <bdank@ucdavis.edu>
2016-01-27 15:56:36 -0800

    click visibility struggle

--- time.js
+++ time.js
@@ -2,29 +2,32 @@
 
 //call to jQuery ready function
 //make sure html and css are loaded before loading js
-$(document).ready(function(){
-   
-    console.log("js ready");
-    
-    //change nav div img opacity to start
-    $('section nav div').css('opacity','0.4');
-    
-    //hover over head image to bring up all bubbles
-    $('section img#head').hover(
-        function(){
-            $('section nav div').css('visibility','visible')},
-        function(){
-        $('section nav div').css('visibility','hidden');
-            return false;
-    }); //end head hover
-    
-    //click head image all bubbles visible
-    $('section img#head').click(
-        function(){
-            $('section nav div')
-                .css('opacity','1')
-                .css('visibility','visible');
+$(document).ready(function () {
+
+        console.log("js ready");
+
+        //change nav div img opacity to start
+        $('section nav div').css('opacity', '0.4');
+
+        //click head image all bubbles visible
+        $('section img#head').click(function () {
+            $('section nav div').css('visibility', 'visible').off('hover');
+        }); //end click head event    
+        //hover over head image to bring up all bubbles
+        $('section img#head').hover(
+            function () {
+                $('section nav div').css('visibility', 'visible')
+            },
+            function () {
+                $('section nav div').css('visibility', 'hidden');
+            }); //end head hover
+
+        //click head image all bubbles visible
+        $('section img#head').click(function () {
+            $('section nav div').toggleClass('toggle');
             return false;
         }); //end click head event
-    
-}) //end ready
+
+        console.log("js ready");
+
+    }) //end ready
