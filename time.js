86b038a024db816c3b55d407a69bfcdca18cc048
bdankul <bdank@ucdavis.edu>
2016-01-27 21:08:43 -0800

    link ex3 portal

--- time.js
+++ time.js
@@ -4,7 +4,7 @@
 //make sure html and css are loaded before loading js
 $(document).ready(function () {
 
-        console.log("js ready");
+        console.log("js ready 1");
 
         //change nav div img opacity to start
         $('section nav div').css('opacity', '0.4');
@@ -18,59 +18,81 @@ $(document).ready(function () {
                 $('section nav div').css('visibility', 'hidden');
                 return false;
             }); //end head hover
-
+    
         //click head image all bubbles visible
         $('section img#head').click(
             function () {
-                $('section nav div').css('opacity', '1')
-                    .css('visibility', 'visible');
-            },
-            function () {
-                $('section img#head').off('hover');
+                $('section nav div')
+                    .css('visibility', 'visible')
+                    .css('opacity', '1.0');
             }
         ); //end click head event
 
-        console.log("js ready");
+        $('section img#head').click(
+            function () {
+                $('aside table').css('visibility', 'visible');
+            });
+        console.log("js ready 2");
+
 
         // bike list trigger bubble
         $('table tr.bike').hover(
             function () {
-                $('section nav div#bike').css('visibility', 'visible')
+                $('section nav div#bike')
+                    .css('visibility', 'visible')
                     .css('opacity', '1');
             },
             function () {
-                $('section nav div#bike').css('visibility', 'hidden');
+                $('section nav div#bike')
+                    .css('visibility', 'hidden')
+                    .css('opacity', '0.6');
+
+                return false;
             }); // end bike trigger
 
         //work list trigger
         $('table tr.work').hover(
             function () {
-                $('section nav div#work').css('visibility', 'visible')
+                $('section nav div#work')
+                    .css('visibility', 'visible')
                     .css('opacity', '1');
             },
             function () {
-                $('section nav div#work').css('visibility', 'hidden');
+                $('section nav div#work')
+                    .css('visibility', 'hidden')
+                    .css('opacity', '0.6');
+                return false;
             }); // end work trigger
 
         //food list trigger
         $('table tr.food').hover(
             function () {
-                $('section nav div#food').css('visibility', 'visible')
+                $('section nav div#food')
+                    .css('visibility', 'visible')
                     .css('opacity', '1');
             },
             function () {
-                $('section nav div#food').css('visibility', 'hidden');
+                $('section nav div#food')
+                    .css('visibility', 'hidden')
+                    .css('opacity', '0.6');
+                return false;
             }); // end food trigger
-    
-            //play list trigger
-        $('table tr.play').hover(
+
+        //play list trigger
+        $('table tr.game').hover(
             function () {
-                $('section nav div#play').css('visibility', 'visible')
+                $('section nav div#game')
+                    .css('transition-timing-function', 'ease')
+                    .css('visibility', 'visible')
                     .css('opacity', '1');
             },
             function () {
-                $('section nav div#play').css('visibility', 'hidden');
-            }); // end food trigger
+                $('section nav div#game')
+                    .css('visibility', 'hidden')
+                    .css('opacity', '0.6');
+                return false;
+            }); // end play trigger
 
-
+        console.log("js ready 3");
+        return false;
     }) //end ready
